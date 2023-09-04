73c025b29e5558f46d24b20d20d794ef15803f13
SSH <12si47bun@gmail.com>
2023-09-04 16:58:11 +0900

    core code

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // Hacker News API Client App lecture
 
 const ajax = new XMLHttpRequest();
-
 // v0/show/뒤에 숫자가 페이지임. 실제 사이트로 보기 : https://news.ycombinator.com/show
 const NEWS_URL = 'https://api.hnpwa.com/v0/show/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+
 ajax.open('GET', NEWS_URL, false); 
 ajax.send();
 
@@ -14,8 +15,14 @@ const ul = document.createElement('ul');
 
 for (let i = 0; i<30; i++) {
   const li = document.createElement('li');
-    li.innerHTML = newsFeed[i].title;
+  const a = document.createElement('a');
+
+    a.href = '#';
+    a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+
+  li.appendChild(a);
   ul.appendChild(li); 
 }
 // appendChild : 하위요소에 태그추가
 document.getElementById('root').appendChild(ul);
+
