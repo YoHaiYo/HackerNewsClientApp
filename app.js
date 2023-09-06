43fd620d6e7358ccf711f626c268c44f9409181a
SSH <12si47bun@gmail.com>
2023-09-06 09:47:42 +0900

    CH03_05. 라우터 화면 처리기 만들기

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const container = document.getElementById('root'); // 재사용을 위해 변수
 const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 // v0/show/뒤에 숫자가 페이지임. 실제 사이트로 보기 : https://news.ycombinator.com/show
-const NEWS_URL = 'https://api.hnpwa.com/v0/show/1.json';
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
 function getData(url) {
@@ -13,37 +13,51 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
+// newsFeed함수 : API URL를 가져와 글 목록을 보여주는 함수.
+// 배열에 ul,li태그를 넣고 join으로 문자열로 바꾸는 방식을 사용.
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL);
+  const newList = [];
 
-// console.log(newsFeed);
-const ul = document.createElement('ul');
+  newList.push('<ul>');
+  
+  for (let i = 0; i<10; i++) {
+    newList.push(`
+    <li>
+      <a href="#${newsFeed[i].id}">
+      ${newsFeed[i].title} (${newsFeed[i].comments_count})
+      </a>
+    </li>
+    `);
+  }
+  newList.push('</ul>')
+  
+  // join : 배열을 ,로 연결된 하나의 문자열로 바꿔줌. 
+  container.innerHTML = newList.join('') ; // 빈문자열로 넣어주면 ,없이 문자열 연결.
+};
 
-window.addEventListener('hashchange', function() { // url의 #이 변할때
+function newsDetail() { // url의 #이 변할때
   const id = location.hash.substring(1); // location : 주소와 관련된 기능제공
   
   const newsContent = getData(CONTENT_URL.replace('@id', id))
-  const title = this.document.createElement('h1');
 
-  title.innerHTML = newsContent.title;
-
-  content.appendChild(title);
-  console.log(newsContent);
-});
-
-for (let i = 0; i<10; i++) {
-  const div = document.createElement('div');
-  
-  div.innerHTML = `
-  <li>
-    <a href="#${newsFeed[i].id}">
-    ${newsFeed[i].title} (${newsFeed[i].comments_count})
-    </a>
-  </li>
+  container.innerHTML = `
+    <h1>${newsContent.title}</h1>
+    <div>
+      <a href="#">목록으로</a>
+    </div>
   `;
-
-  ul.appendChild(div.firstElementChild); 
 }
-// appendChild : 하위요소에 태그추가
-container.appendChild(ul);
-container.appendChild(content);
 
+function router() {
+  const routePath = location.hash; //location.hash에 #만 있으면 빈값을 반환.
+
+  if (routePath === ''){
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+
+window.addEventListener('hashchange', router);
+router();
